a5fe367d6adc5452bdf4630729c12784b64f45e1
emilieagramonte <133388293+emilieagramonte@users.noreply.github.com>
2023-07-19 21:33:39 -0400

    lifedb ejemplo getting started

---
+++ GETTING STARTED 2/Program.cs
@@ -0,0 +1,63 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GETTING_STARTED_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Open database (or create if doesn't exist)
+            using (var db = new LiteDatabase(@"C:\Users\PC\Documents\GETTING STARTED 2\MyData.db"))
+            {
+                // Get a collection (or create, if doesn't exist)
+                var col = db.GetCollection<Customer>("customers");
+
+                // Create your new customer instance
+                var customer = new Customer
+                {
+                    Name = "John Doe",
+                    Phones = new string[] { "8000-0000", "9000-0000" },
+                    IsActive = true
+                };
+
+                // Insert new customer document (Id will be auto-incremented)
+                col.Insert(customer);
+
+                // Update a document inside a collection
+                customer.Name = "Jane Doe";
+
+                col.Update(customer);
+
+                // Index document using document Name property
+                col.EnsureIndex(x => x.Name);
+
+                // Use LINQ to query documents (filter, sort, transform)
+                var results = col.Query()
+                    .Where(x => x.Name.StartsWith("J"))
+                    .OrderBy(x => x.Name)
+                    .Select(x => new { x.Name, NameUpper = x.Name.ToUpper() })
+                    .Limit(10)
+                    .ToList();
+
+                // Let's create an index in phone numbers (using expression). It's a multikey index
+                col.EnsureIndex(x => x.Phones);
+
+                // and now we can query phones
+                var r = col.FindOne(x => x.Phones.Contains("8888-5555"));
+            }
+        }
+    }
+    // Create your POCO class entity
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] Phones { get; set; }
+        public bool IsActive { get; set; }
+    }
+} 
